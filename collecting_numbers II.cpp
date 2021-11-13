90bd9f42dbc8ba2ef54aafb4f5410f62cc4cfb3e
samiya <64473932+samiya12-hash@users.noreply.github.com>
2021-11-13 14:04:35 +0600

    Update collecting_numbers II.cpp

--- collecting_numbers II.cpp
+++ collecting_numbers II.cpp
@@ -43,8 +43,8 @@ int main()
 	    IOS;
 	// freopen("input.txt","r",stdin);
     // freopen("output.txt","w",stdout);
-   ll n,m;cin>>n>>m;
-   ll ma[n];
+ ll n,m;cin>>n>>m;
+   ll ma[n+1];
    memset(ma,0,sizeof(ma));
    vi v(n);
    ll ans = 1;
@@ -59,60 +59,25 @@ int main()
    ll a,b;cin>>a>>b;
    ll numa = v[a-1];
    ll numb = v[b-1];
-   // 1st number checking
-       // curr - > prev
-       if(numa!=1 && ma[numa]<ma[numa-1])
-       {
-           ans--;
-        //    cout<<"ds"<<endl;
-       }
-       if(numa!=n && ma[numa]>ma[numa+1])
-       {
-        //    cout<<"ps"<<endl;
-           ans--;
-       }
-       if(numb!=1 && ma[numb]<ma[numb-1])
-       {
-           if(numa!=numb-1)
-           {
-            //    cout<<"d"<<endl;
-               ans--;
-           }
-       }
-       if(numb!=n && ma[numb]>ma[numb+1])
-       {
-           if(numa!=numb+1)
-           {
-            //    cout<<"saw"<<endl;
-               ans--;
-           }
-       }
-    //    cout<<ans<<endl;
-      ma[numa] = b;
-       ma[numb] = a;
-       ll cng = v[a-1];
-       v[a-1] = v[b-1];
-       v[b-1] = cng;
-       // ma[4] = 5 , ma[3] = 1
-    //    cout<<numa<<" "<<ma[numa]<<endl;
-       if(numa!=1 && ma[numa]<ma[numa-1])ans++;
-       if(numa!=n && ma[numa]>ma[numa+1])ans++;
-       if(numb!=1 && ma[numb]<ma[numb-1] )
-       {
-           if(numa!=numb-1)
-           {
-            //    cout<<"d"<<endl;
-               ans++;
-           }
-       }
-       if(numb!=n && ma[numb]>ma[numb+1]) {
-           if(numa!=numb+1)
-           {
-            //    cout<<"p"<<endl;
-               ans++;
-           }
-       }
-       cout<<ans<<endl;
+   set<pii> s;
+   // x<y
+   if(numa!=1)s.insert({numa-1,numa});
+   if(numa!=n)s.insert({numa,numa+1});
+   if(numb!=1)s.insert({numb-1,numb});
+   if(numb!=n)s.insert({numb,numb+1});
+   for(auto x:s)
+   {
+       if(ma[x.first]>ma[x.second])ans--;
+   }
+   v[a-1] = numb;
+   v[b-1] = numa;
+   ma[numb] = a;
+   ma[numa] = b;
+   for(auto x:s)
+   {
+       if(ma[x.first]>ma[x.second])ans++;
+   }
+   cout<<ans<<endl;
    }
    return 0;
 }          
